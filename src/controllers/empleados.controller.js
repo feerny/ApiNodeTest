64799e8cd4196f715af1b67a46d392e3724766ee
feerny <luisfpe19@gmail.com>
2023-01-05 14:39:15 -0500

    pruebas

--- src/controllers/empleados.controller.js
+++ src/controllers/empleados.controller.js
@@ -80,7 +80,7 @@ export const putEmpleados = async (req, res) => {
         call employeedAddOrEdit(?,?,?);
     `
     try {
-        const [rows] = await mysqlConecction.query(query, [id, name, salary])
+        const rows = await mysqlConecction.query(query, [id, name, salary])
         console.log(rows);
         if (rows[1].affectedRows !== 0) {
             return res.json({ Status: `empleado ${id} Actualizado` })
