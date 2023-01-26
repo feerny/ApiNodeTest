afff12eea4c642f61fbe8ea317d4b7e83840e381
feerny <luisfpe19@gmail.com>
2023-01-26 14:58:59 -0500

    termine subir img a cloudinary, falta configurar put y path

--- src/controllers/empleados.controller.js
+++ src/controllers/empleados.controller.js
@@ -1,148 +1,153 @@
-const  mysqlConecction=require('../database.js')
+const mysqlConecction = require('../database.js')
 
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require("../utils/cloudinary");
 
-module.exports={
+module.exports = {
 
- getEmpleados : async(req, res) => {
-
-    try {
-        const [rows] = await mysqlConecction.query('select * from employees')
-
-        const bodyJson={
-            id:rows.id,
-            name:rows.name,
-            salary:rows.salary,
-            img:JSON.parse(rows.img)
-        }
-
-        if (rows.length !== 0) {
-            return res.json(bodyJson)
-        } else {
-            return res.status(404).json({ Status: "empleados no encontrados" })
-
-        
-        }
-    } catch (error) {
-        const errors = `${error}`;
-        console.log(errors.split(" ").slice(1).join(" "));
-        return res.status(500).json({ message: `${errors.split(" ").slice(1).join(" ")}` })
-    }
-
-},
-
-
-
- getEmpleadosId : async (req, res) => {
-
-    try {
-        const { id } = req.params
-        const [rows] = await mysqlConecction.query('select * from employees where id=?', [id])
-
-        if (rows.length !== 0) {
-            return res.json(rows[0])
-        } else {
-            return res.status(404).json({ Status: "empleado no encontrado" })
-
-
-        }
-    } catch (error) {
-        const errors = `${error}`;
-        console.log(errors.split(" ").slice(1).join(" "));
-        return res.status(500).json({ message: `${errors.split(" ").slice(1).join(" ")}` })
-    }
-
-},
-
- postEmpleados : async (req, res) => {
-
-    
-    try {
-        const { name, salary } = req.body;
-        if (!req.file) {
-            return res.status(500).json({ message: "porfavor suba una imagen"})
-        }
+    getEmpleados: async (req, res) => {
 
         try {
-            var cloudinary_image=await cloudinary.uploader.upload(req.file.path,{
-                folder:'empleados',
-            });
+            const [rows] = await mysqlConecction.query('select * from employees')
+
+            const imgParse = JSON.parse(rows[0].img)
+
+            const myJson = {
+                id: rows[0].id,
+                name: rows[0].name,
+                salary: rows[0].salary,
+                image: imgParse
+            }
+            if (rows.length !== 0) {
+                return res.json(myJson)
+            } else {
+                return res.status(404).json({ Status: "empleados no encontrados" })
+
+
+            }
         } catch (error) {
-            console.log(error);
-            return res.status(500).json({ message: error})
+            const errors = `${error}`;
+            console.log(errors.split(" ").slice(1).join(" "));
+            return res.status(500).json({ message: `${errors.split(" ").slice(1).join(" ")}` })
         }
 
-        const query = `
+    },
+
+
+
+    getEmpleadosId: async (req, res) => {
+
+        try {
+            const { id } = req.params
+            const [rows] = await mysqlConecction.query('select * from employees where id=?', [id])
+
+            if (rows.length !== 0) {
+                return res.json(rows[0])
+            } else {
+                return res.status(404).json({ Status: "empleado no encontrado" })
+
+
+            }
+        } catch (error) {
+            const errors = `${error}`;
+            console.log(errors.split(" ").slice(1).join(" "));
+            return res.status(500).json({ message: `${errors.split(" ").slice(1).join(" ")}` })
+        }
+
+    },
+
+    postEmpleados: async (req, res) => {
+
+
+        try {
+            const { name, salary } = req.body;
+            if (!req.file) {
+                return res.status(500).json({ message: "porfavor suba una imagen" })
+            }
+
+            try {
+                var cloudinary_image = await cloudinary.uploader.upload(req.file.path, {
+                    folder: 'empleados',
+                });
+            } catch (error) {
+                console.log(error);
+                return res.status(400).json({ message: error })
+            }
+
+            const query = `
         call employeedAddOrEdit(?,?,?,?);
         `
-        const image={
-            public_id:cloudinary_image.public_id,
-            url:cloudinary_image.secure_url
+            const image = {
+                public_id: `${cloudinary_image.public_id}`,
+                url: `${cloudinary_image.secure_url}`
+            }
+
+            console.log(image);
+
+            const imageParse = JSON.stringify(image);
+            console.log(imageParse);
+            const [rows] = await mysqlConecction.query(query, [0, imageParse, name, salary])
+            console.log(rows);
+
+            if (rows[0][0].id === null || rows[0][0].id === 0) {
+                return res.status(500).json({ Status: `error al crear empleado` })
+
+            }
+            else {
+                return res.json({
+                    Status: "empleado Guardado",
+                    id: rows[0][0].id,
+                    name,
+                    salary
+                })
+            }
+        } catch (error) {
+            const errors = `${error}`;
+            console.log(errors.split(" ").slice(1).join(" "));
+            return res.status(500).json({ message: `${errors.split(" ").slice(1).join(" ")}` })
         }
 
-        const imageParse = JSON.stringify( image );
-        const [rows] = await mysqlConecction.query(query, [0,name, salary,imageParse])
-        console.log(rows);
 
-        if (rows[0][0].id === null || rows[0][0].id === 0) {
-            return res.status(500).json({ Status: `error al crear empleado` })
+    },
 
-        }
-        else {
-            return res.json({
-                Status: "empleado Guardado",
-                id: rows[0][0].id,
-                name,
-                salary
-            })
-        }
-    } catch (error) {
-        const errors = `${error}`;
-        console.log(errors.split(" ").slice(1).join(" "));
-        return res.status(500).json({ message: `${errors.split(" ").slice(1).join(" ")}` })
-    }
-
-
-},
-
- putEmpleados : async (req, res) => {
-    const { id } = req.params
-    const { name, salary } = req.body;
-    const query = `
+    putEmpleados: async (req, res) => {
+        const { id } = req.params
+        const { name, salary } = req.body;
+        const query = `
     call employeedAddOrEdit(?,?,?);
     `
-    try {
-        const [rows] = await mysqlConecction.query(query, [id,name, salary])
-        console.log(rows);
-        if (rows.affectedRows !== 0) {
-            return res.json({ Status: `empleado ${id} Actualizado` })
-        } else {
-            return res.status(500).json({ Status: "empleado no Actualizado o no existente" })
+        try {
+            const [rows] = await mysqlConecction.query(query, [id, name, salary])
+            console.log(rows);
+            if (rows.affectedRows !== 0) {
+                return res.json({ Status: `empleado ${id} Actualizado` })
+            } else {
+                return res.status(500).json({ Status: "empleado no Actualizado o no existente" })
 
+            }
+        } catch (error) {
+            const errors = `${error}`;
+            console.log(errors.split(" ").slice(1).join(" "));
+            return res.status(500).json({ message: `${errors.split(" ").slice(1).join(" ")}` })
         }
-    } catch (error) {
-        const errors = `${error}`;
-        console.log(errors.split(" ").slice(1).join(" "));
-        return res.status(500).json({ message: `${errors.split(" ").slice(1).join(" ")}` })
-    }
-},
+    },
 
- deleteEmpleado: async(req, res) =>{
-    try {
-        const { id } = req.params
+    deleteEmpleado: async (req, res) => {
+        try {
+            const { id } = req.params
 
-        const [rows] = await mysqlConecction.query("delete from employees where id=?", [id])
+            const [rows] = await mysqlConecction.query("delete from employees where id=?", [id])
 
-        if (rows.affectedRows !== 0) {
-            return res.json({ Status: `empleado ${id} Eliminado` })
-        } else {
-            return res.status(500).json({ Status: "empleado no Eliminado o no existente" })
+            if (rows.affectedRows !== 0) {
+                return res.json({ Status: `empleado ${id} Eliminado` })
+            } else {
+                return res.status(500).json({ Status: "empleado no Eliminado o no existente" })
 
+            }
+        } catch (error) {
+            const errors = `${error}`;
+            console.log(errors.split(" ").slice(1).join(" "));
+            return res.status(500).json({ message: `${errors.split(" ").slice(1).join(" ")}` })
         }
-    } catch (error) {
-        const errors = `${error}`;
-        console.log(errors.split(" ").slice(1).join(" "));
-        return res.status(500).json({ message: `${errors.split(" ").slice(1).join(" ")}` })
-    }
 
-}}
+    }
+}
